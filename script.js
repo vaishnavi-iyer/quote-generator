b3d05b17564983151bb7d1af77b72226a2e4b284
vaishnavi-iyer <iyervaishnavi26@gmail.com>
2016-05-22 23:06:00 +1200

    logic to make it purely random

--- script.js
+++ script.js
@@ -20,26 +20,18 @@ var thoughts = [
 {"quote": "dfhjgfhkhjgkjhkj",
 "from": "ghfghjfghjf"}
 ];
+
+var lastQuoteNum;
+
 function loadQuote ()  // function to load a new quote
 {
   var quoteNum = Math.floor(Math.random()*(thoughts.length));    /// to generate a random quote number
-
+  if(lastQuoteNum===quoteNum)
+    quoteNum++;
   console.log(thoughts[quoteNum])
   $("#quote").empty().append(thoughts[quoteNum].quote);
   $("#from").empty().append(thoughts[quoteNum].from);
-
   //document.getElementById('quote').innerHTML = thoughts[quoteNum].quote; // using js
-}
 
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
+  lastQuoteNum = quoteNum
 }
-
