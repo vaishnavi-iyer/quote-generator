129a44ec0a41f41a9845055300f47043e3e9f76c
vaishnavi-iyer <iyervaishnavi26@gmail.com>
2016-05-23 22:46:17 +1200

    added twitter link

--- script.js
+++ script.js
@@ -9,19 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {// to make syre script
 });
 
 
-
 var thoughts = [
-{"quote": "afhjgfhkhjgkjhkj",
-"from": "ghfghjfghjf"},
-{"quote": "bfhjgfhkhjgkjhkj",
-"from": "ghfghjfghjf"},
-{"quote": "cfhjgfhkhjgkjhkj",
-"from": "ghfghjfghjf"},
-{"quote": "dfhjgfhkhjgkjhkj",
-"from": "ghfghjfghjf"}
+{"quote": "Another world is not only possible, she is on her way. On a quiet day, I can hear her breathing.",
+"from": "Arundhati Roy"},
+{"quote": "If this is the best of possible worlds, what then are the others?",
+"from": "Voltaire"},
+{"quote": "Being challenged in life is inevitable, being defeated is optional.",
+"from": "Roger Crawford"},
+{"quote": "Don't cry because it's over, smile because it happened.",
+"from": "Dr Seuss"},
+{"quote": "We are all in the gutter, but some of us are looking at the stars.",
+"from": "Oscar Wilde"},
+{"quote": "How wonderful it is that nobody need wait a single moment before starting to improve the world.",
+"from": "Anne Frank"},
+{"quote": "A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.",
+"from": "Winston Churchill"},
+{"quote": "Dwell in possibility",
+"from": "Emiily Dickinson"},
+{"quote": "Write it on your heart that every day is the best day in the year.",
+"from": "Ralph Waldo Emerson"},
+{"quote": "You'll never find a rainbow if you're looking down",
+"from": "Charlie Chaplin"}
 ];
 
-var lastQuoteNum;
+var lastQuoteNum, tweetLink;
 
 function loadQuote ()  // function to load a new quote
 {
@@ -34,4 +45,6 @@ function loadQuote ()  // function to load a new quote
   //document.getElementById('quote').innerHTML = thoughts[quoteNum].quote; // using js
 
   lastQuoteNum = quoteNum
+
+  tweetLink = "https://twitter.com/intent/tweet?hashtags=thoughtoftheday&text=" + thoughts[quoteNum].quote + " - " + thoughts[quoteNum].from;
 }
